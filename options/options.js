ac76be4013238c908e38556237a6d147f91dd107
Crowdbotics <team@crowdbotics.com>
2024-03-05 02:08:05 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-river-46807",
+  url: "https://twilight-river-46807.botics.co",
+  api: "https://twilight-river-46807.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
